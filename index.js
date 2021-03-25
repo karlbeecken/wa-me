8dde86c48bede9ec6d8b3dfb5594989077b6ab84
Karl Beecken <karl@beecken.berlin>
2021-03-25 19:48:49 +0100

    ✨ add generator with pre-filled message and number

--- index.js
+++ index.js
@@ -6,3 +6,14 @@ exports.createFromNumber = (number) => {
   number = number.replace(/[^0-9]/g, "");
   return `https://wa.me/${number}`;
 };
+
+/**
+ * Create a wa.me link from a phone number with a pre-filled message
+ * @param {string} number The phone number to be converted, must be a full number including country code
+ * @param {string} message The message to be encoded into the link
+ */
+exports.createFromNumberWithMessage = (number, message) => {
+  number = number.replace(/[^0-9]/g, "");
+  message = encodeURIComponent(message);
+  return `https://wa.me/${number}?text=${message}`;
+};

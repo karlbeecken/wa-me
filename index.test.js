a0891173558d37110899fcf11738488127309b36
Karl Beecken <karl@beecken.berlin>
2021-03-25 19:55:35 +0100

    ✅ add tests for pre-filled-message generator

--- index.test.js
+++ index.test.js
@@ -7,3 +7,19 @@ describe("Create a wa.me link from a phone number", () => {
     expect(waMe.createFromNumber(input)).toEqual(output);
   });
 });
+
+describe("Create a wa.me link from a phone number with a pre-filled message", () => {
+  test("it should filter weird chars from the phone number input", () => {
+    const number = "+49 (30) 82-56/123 99";
+    const message = "";
+    const output = "https://wa.me/4930825612399?text=";
+    expect(waMe.createFromNumberWithMessage(number, message)).toEqual(output);
+  });
+  test("it should URL-encode the pre-filled message", () => {
+    const number = "+49 (30) 82-56/123 99";
+    const message = "❤✊😉";
+    const output =
+      "https://wa.me/4930825612399?text=%E2%9D%A4%E2%9C%8A%F0%9F%98%89";
+    expect(waMe.createFromNumberWithMessage(number, message)).toEqual(output);
+  });
+});

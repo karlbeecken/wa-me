a0fbfa4f2dac6358f68ee9ca07687d3cafd9eaaf
Karl Beecken <karl@beecken.berlin>
2021-03-25 21:14:50 +0100

    ✨ add predefined country code feature

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 /**
  * Create a wa.me link from a phone number
  * @param {string} number The phone number to be converted, must be a full number including country code
+ * @param {string} [countryCode] The default country code which should be applied if the number begins with a 0
  */
-exports.createFromNumber = (number) => {
+exports.createFromNumber = (number, countryCode) => {
+  if (countryCode) {
+    number = number.replace(/^0/, countryCode);
+  }
   number = number.replace(/[^0-9]/g, "");
   return `https://wa.me/${number}`;
 };
@@ -11,8 +15,12 @@ exports.createFromNumber = (number) => {
  * Create a wa.me link from a phone number with a pre-filled message
  * @param {string} number The phone number to be converted, must be a full number including country code
  * @param {string} message The message to be encoded into the link
+ * @param {string} [countryCode] The default country code which should be applied if the number begins with a 0, e.g. 49 for Germany
  */
-exports.createFromNumberWithMessage = (number, message) => {
+exports.createFromNumberWithMessage = (number, message, countryCode) => {
+  if (countryCode) {
+    number = number.replace(/^0/, countryCode);
+  }
   number = number.replace(/[^0-9]/g, "");
   message = encodeURIComponent(message);
   return `https://api.whatsapp.com/send/?phone=${number}&text=${message}`;
--- index.test.js
+++ index.test.js
@@ -2,9 +2,21 @@ const waMe = require("./index");
 
 describe("Create a wa.me link from a phone number", () => {
   test("it should filter weird chars from the phone number input", () => {
-    const input = "+49 (30) 82-56/123 99";
+    const number = "+49 (30) 82-56/123 99";
     const output = "https://wa.me/4930825612399";
-    expect(waMe.createFromNumber(input)).toEqual(output);
+    expect(waMe.createFromNumber(number)).toEqual(output);
+  });
+  test("it should add a country code instead of the leading 0", () => {
+    const number = "030 82-56/123 99";
+    const countryCode = "49";
+    const output = "https://wa.me/4930825612399";
+    expect(waMe.createFromNumber(number, countryCode)).toEqual(output);
+  });
+  test("it should not add a country code instead of the leading 0 if a country code is already present", () => {
+    const number = "+49 (30) 82-56/123 99";
+    const countryCode = "49";
+    const output = "https://wa.me/4930825612399";
+    expect(waMe.createFromNumber(number, countryCode)).toEqual(output);
   });
 });
 
@@ -14,6 +26,18 @@ describe("Create a wa.me link from a phone number with a pre-filled message", ()
     const message = "";
     const output = "https://api.whatsapp.com/send/?phone=4930825612399&text=";
     expect(waMe.createFromNumberWithMessage(number, message)).toEqual(output);
+  });
+  test("it should add a country code instead of the leading 0", () => {
+    const number = "030 82-56/123 99";
+    const countryCode = "49";
+    const output = "https://wa.me/4930825612399";
+    expect(waMe.createFromNumber(number, countryCode)).toEqual(output);
+  });
+  test("it should not add a country code instead of the leading 0 if a country code is already present", () => {
+    const number = "+49 (30) 82-56/123 99";
+    const countryCode = "49";
+    const output = "https://wa.me/4930825612399";
+    expect(waMe.createFromNumber(number, countryCode)).toEqual(output);
   });
   test("it should URL-encode the pre-filled message", () => {
     const number = "+49 (30) 82-56/123 99";
